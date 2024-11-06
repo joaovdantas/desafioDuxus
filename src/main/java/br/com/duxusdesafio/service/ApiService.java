a4b254f8b611bc254a9d12eac91d6ae753a735d8
name <joaovitordasilvadantas@outlook.com>
2024-11-06 12:56:37 -0300

    feat add full method

--- src/main/java/br/com/duxusdesafio/service/ApiService.java
+++ src/main/java/br/com/duxusdesafio/service/ApiService.java
@@ -1,79 +1,97 @@
 package br.com.duxusdesafio.service;
 
-import br.com.duxusdesafio.model.Integrante;
-import br.com.duxusdesafio.model.Time;
-import org.springframework.stereotype.Service;
-
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-/**
- * Service que possuirá as regras de negócio para o processamento dos dados
- * solicitados no desafio!
- *
- * @author carlosau
- */
+import org.springframework.stereotype.Service;
+
+import br.com.duxusdesafio.model.ComposicaoTime;
+import br.com.duxusdesafio.model.Integrante;
+import br.com.duxusdesafio.model.Time;
+
 @Service
 public class ApiService {
+	//Vai retornar uma lista com os nomes dos integrantes do time daquela data
+	public List<Integrante> timeDaData(LocalDate data, List<Time> todosOsTimes) {
+		List<Integrante> integrantes = new ArrayList<>();/* Instanciando uma lista dos integrantes */
+		for (Time time : todosOsTimes) { /* verificando todos os times na lista */
+			if (time.getData().isEqual(data)) {/* Verificando se a data digitada é a mesma do time */
+				for (ComposicaoTime composicao : time.getComposicaoTime()) {/* busquei acessar o atributo integrante que ta no obj Composiçao */
+					integrantes.add(
+							composicao.getIntegrante()); /* Adicionando o atributo integrante à lista de integrantes */
 
-    /**
-     * Vai retornar uma lista com os nomes dos integrantes do time daquela data
-     */
-    public Time timeDaData(LocalDate data, List<Time> todosOsTimes){
-        // TODO Implementar método seguindo as instruções!
-        return null;
-    }
+				}
+			}
+		}
+		return integrantes; // Retornando o meu metodo que é uma lista com os integrantes do time daquela data
+							
+	}
 
-    /**
-     * Vai retornar o integrante que tiver presente na maior quantidade de times
-     * dentro do período
-     */
-    public Integrante integranteMaisUsado(LocalDate dataInicial, LocalDate dataFinal, List<Time> todosOsTimes){
-        // TODO Implementar método seguindo as instruções!
-        return null;
-    }
+	// Retornar o integrante que tiver presente na maior quantidade de times dentro do período
+	
+	public Integrante integranteMaisUsado(LocalDate dataInicial, LocalDate dataFinal, List<Time> todosOsTimes) {
+		HashMap<Integrante,Integer> contagem = new HashMap<>();
+		for (Time time : todosOsTimes) {
+			 if (time.getData().isAfter(dataInicial) && time.getData().isBefore(dataFinal)) {
+				 for (ComposicaoTime composicao : time.getComposicaoTime()) {/* Acessando todos obj retornados pelo getComposicaoTime e alocando no composicao*/
+		                Integrante jogador = composicao.getIntegrante();
+		               contagem.put(jogador, contagem.getOrDefault(jogador,0) + 1); /* Na contagem está adicionando o jogador partindo do zero */
+		             
+				 }
+			 }
+		}
+        Integrante jogadorMaispresente = null; //Criando variavel do tipo Integrante pra verificar o jogador, iniciado com null porque seu tipo tem que armazenar o jogador obj e nao o numero.
 
-    /**
-     * Vai retornar uma lista com os nomes dos integrantes do time mais comum
-     * dentro do período
-     */
-    public List<String> timeMaisComum(LocalDate dataInicial, LocalDate dataFinal, List<Time> todosOsTimes){
-        // TODO Implementar método seguindo as instruções!
-        return null;
-    }
+        for (Map.Entry<Integrante, Integer > entry : contagem.entrySet()) {// no mapa estou acessando a chave e o valor com o metodo Entryset que faz parte do hashmap
+            if(entry.getValue()>0){//se qualquer valor for atribuito a contagem que no caso é 0. entao toda entrando sendo maior q o padrao  fara.
+                jogadorMaispresente = entry.getKey(); //variavel instanciada recebe o jogador ou seja a key.
+            }
+        }
+        return jogadorMaispresente;//retornando o jogador mais presente
+	}
 
-    /**
-     * Vai retornar a função mais comum nos times dentro do período
-     */
-    public String funcaoMaisComum(LocalDate dataInicial, LocalDate dataFinal, List<Time> todosOsTimes){
-        // TODO Implementar método seguindo as instruções!
-        return null;
-    }
+	/**
+	 * Vai retornar uma lista com os nomes dos integrantes do time mais comum dentro
+	 * do período
+	 */
+	public List<String> timeMaisComum(LocalDate dataInicial, LocalDate dataFinal, List<Time> todosOsTimes) {
+		// TODO Implementar método seguindo as instruções!
+		return null;
+	}
 
-    /**
-     * Vai retornar o nome da Franquia mais comum nos times dentro do período
-     */
-    public String franquiaMaisFamosa(LocalDate dataInicial, LocalDate dataFinal, List<Time> todosOsTimes) {
-        // TODO Implementar método seguindo as instruções!
-        return null;
-    }
+	/**
+	 * Vai retornar a função mais comum nos times dentro do período
+	 */
+	public String funcaoMaisComum(LocalDate dataInicial, LocalDate dataFinal, List<Time> todosOsTimes) {
+		// TODO Implementar método seguindo as instruções!
+		return null;
+	}
 
+	/**
+	 * Vai retornar o nome da Franquia mais comum nos times dentro do período
+	 */
+	public String franquiaMaisFamosa(LocalDate dataInicial, LocalDate dataFinal, List<Time> todosOsTimes) {
+		// TODO Implementar método seguindo as instruções!
+		return null;
+	}
 
-    /**
-     * Vai retornar o nome da Franquia mais comum nos times dentro do período
-     */
-    public Map<String, Long> contagemPorFranquia(LocalDate dataInicial, LocalDate dataFinal, List<Time> todosOsTimes){
-        // TODO Implementar método seguindo as instruções!
-        return null;
-    }
+	/**
+	 * Vai retornar o nome da Franquia mais comum nos times dentro do período
+	 */
+	public Map<String, Long> contagemPorFranquia(LocalDate dataInicial, LocalDate dataFinal, List<Time> todosOsTimes) {
+		// TODO Implementar método seguindo as instruções!
+		return null;
+	}
 
-    /**
-     * Vai retornar o número (quantidade) de Funções dentro do período
-     */
-    public Map<String, Long> contagemPorFuncao(LocalDate dataInicial, LocalDate dataFinal, List<Time> todosOsTimes){
-        // TODO Implementar método seguindo as instruções!
-        return null;
-    }
+	/**
+	 * Vai retornar o número (quantidade) de Funções dentro do período
+	 */
+	public Map<String, Long> contagemPorFuncao(LocalDate dataInicial, LocalDate dataFinal, List<Time> todosOsTimes) {
+		// TODO Implementar método seguindo as instruções!
+		return null;
+	}
 
 }
